7e669b181edc98a016aae3caea23d67a28bf104c
tauseefhaider1 <tauseefhaider119@gmail.com>
2026-01-14 15:26:58 +0500

    Update frontend: connect backend search

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
 ////////////////////
   const checkAuth = async () => {
     try {
-      const res = await api.get("/auth/check");
+      const res = await api.get("/auth/me");
       setUser(res.data.user);
     } catch {
       setUser(null);
